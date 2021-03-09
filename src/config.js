5922b7c4d03c6ddd2f0822eaf412e57a7de1fbc1
Pablo Saba <sabaliauskaspablo@gmail.com>
2021-03-09 18:54:00 -0300

    fix lint problems

--- src/config.js
+++ src/config.js
@@ -1,10 +1,8 @@
 const apis = {
   holidays: 'https://nolaborables.com.ar/api/v2/feriados/{year}?incluir=opcional',
 };
-
 const apisCA = {
   holidays: 'https://canada-holidays.ca/api/v1/provinces/QC?year={year}'
 };
-
 module.exports = { apis, apisCA };
 
