5451ce90f426e022fb0754894021e020fcb78ca7
Leandro Di Lorenzo <leandro.jdl@gmail.com>
2023-03-24 13:29:55 -0300

    feat: add openai

--- src/config.ts
+++ src/config.ts
@@ -6,6 +6,25 @@ export const apisCA = {
   holidays: 'https://canada-holidays.ca/api/v1/provinces/QC?year={year}',
 };
 
-const config = { apis, apisCA };
+export const mainChannel = Number.parseInt(process.env.MAIN_CHANNEL || '-1', 10);
+
+export const openAI = {
+  apiKey: process.env.OPENAI_API_KEY,
+  defaultOptions: {
+    model: 'text-davinci-003',
+    temperature: 0.7,
+    max_tokens: 256,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+  },
+};
+
+const config = {
+  apis,
+  apisCA,
+  mainChannel,
+  openAI,
+};
 
 export default config;
