ee7ed9ccceef4f9a7ead202f8f726a96f1d0d865
Leandro Di Lorenzo <leandro.jdl@gmail.com>
2023-04-15 11:54:43 -0300

    chore: fix ngrok tunnel parse

--- .scripts/echo-tunnel.sh
+++ .scripts/echo-tunnel.sh
@@ -2,4 +2,6 @@
 
 set -x
 
-curl --silent http://127.0.0.1:4040/api/tunnels | grep --only-matching --extended-regexp "https:\/\/[a-zA-z0-9-]+.ngrok.io"
+curl --silent http://127.0.0.1:4040/api/tunnels \
+  | jq --raw-output \
+    '.tunnels[] | select(.public_url | contains("https")) | .public_url'
