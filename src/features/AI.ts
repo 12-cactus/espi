0efe747d3d1411945b60e76c6d094db82fdc33c7
Leandro Di Lorenzo <leandro.jdl@gmail.com>
2023-10-14 10:52:05 -0300

    feat: openai code migration

--- src/features/AI.ts
+++ src/features/AI.ts
@@ -1,23 +1,23 @@
-import { ChatCompletionRequestMessage, Configuration, OpenAIApi } from 'openai';
+import OpenAI from 'openai';
+import { ChatCompletionMessageParam } from 'openai/resources';
 
 import { openAI } from '../config';
 
-const configuration = new Configuration({ apiKey: openAI.apiKey });
-const openai = new OpenAIApi(configuration);
+const openai = new OpenAI({ apiKey: openAI.apiKey });
 
-const ask = async (question: string, context?: ChatCompletionRequestMessage) => {
-  const messages: ChatCompletionRequestMessage[] = [
+const ask = async (question: string, context?: ChatCompletionMessageParam) => {
+  const messages: ChatCompletionMessageParam[] = [
     { role: 'system', content: 'Sus un bot de Telegram llamado Espi. Estás en el grupo "12 Cactus".' },
   ];
   if (context) messages.push(context);
   messages.push({ role: 'user', content: question });
 
-  const response = await openai.createChatCompletion({
+  const response = await openai.chat.completions.create({
     ...openAI.defaultOptions,
     messages,
   });
 
-  const answer = response.data.choices[0].message?.content || '';
+  const answer = response.choices[0].message?.content || '';
   return answer || 'Ups... no se que decirte';
 };
 
